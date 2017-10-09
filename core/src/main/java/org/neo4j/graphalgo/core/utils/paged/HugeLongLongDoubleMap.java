2a741c0142f313e3db2b7533c6e7a06b53b778ee
Paul Horn <knutwalker@users.noreply.github.com>
2017-10-09 12:57:07 +0200

    Fix non-completion bug in Huge WeightMapping (#419)

--- core/src/main/java/org/neo4j/graphalgo/core/utils/paged/HugeLongLongDoubleMap.java
+++ core/src/main/java/org/neo4j/graphalgo/core/utils/paged/HugeLongLongDoubleMap.java
@@ -1,34 +1,37 @@
 package org.neo4j.graphalgo.core.utils.paged;
 
-import org.neo4j.graphalgo.core.utils.container.LongLongDoubleHashMap;
+import com.carrotsearch.hppc.LongDoubleMap;
+import org.neo4j.graphalgo.core.utils.container.TrackingLongDoubleHashMap;
 
+import static com.carrotsearch.hppc.HashContainers.DEFAULT_LOAD_FACTOR;
 import static org.neo4j.graphalgo.core.utils.paged.MemoryUsage.BYTES_OBJECT_REF;
 import static org.neo4j.graphalgo.core.utils.paged.MemoryUsage.shallowSizeOfInstance;
 import static org.neo4j.graphalgo.core.utils.paged.MemoryUsage.sizeOfDoubleArray;
 import static org.neo4j.graphalgo.core.utils.paged.MemoryUsage.sizeOfLongArray;
 
 
-public final class HugeLongLongDoubleMap extends PagedDataStructure<LongLongDoubleHashMap> {
+public final class HugeLongLongDoubleMap extends PagedDataStructure<HugeLongLongDoubleMap.NestedMap> {
 
 
-    private static final PageAllocator.Factory<LongLongDoubleHashMap> ALLOCATOR_FACTORY;
+    private static final PageAllocator.Factory<NestedMap> ALLOCATOR_FACTORY;
 
     static {
         int pageSize = PageUtil.pageSizeFor(BYTES_OBJECT_REF);
-        int bufferLength = BitUtil.nextHighestPowerOfTwo(pageSize) << 1;
-        long keyUsage = sizeOfLongArray(bufferLength);
-        long valueUsage = sizeOfDoubleArray(bufferLength);
 
-        long pageUsage = shallowSizeOfInstance(LongLongDoubleHashMap.class);
-        pageUsage += keyUsage; // keys1
-        pageUsage += keyUsage; // keys2
-        pageUsage += valueUsage; // values
+        // can't really estimate memory usage as it depends on degree distribution
+        // just pretend every node is connected to 16 other nodes and go from there
+        long perNodeUsage = shallowSizeOfInstance(TrackingLongDoubleHashMap.class)
+                + sizeOfLongArray(33)    // keys
+                + sizeOfDoubleArray(33); // values
+
+        long pageUsage = shallowSizeOfInstance(NestedMap.class);
+        pageUsage += (perNodeUsage * pageSize);
 
         ALLOCATOR_FACTORY = PageAllocator.of(
                 pageSize,
                 pageUsage,
-                () -> new LongLongDoubleHashMap(pageSize),
-                new LongLongDoubleHashMap[0]);
+                (tracker) -> new NestedMap(pageSize, tracker),
+                new NestedMap[0]);
     }
 
     public static long estimateMemoryUsage(long size) {
@@ -39,19 +42,57 @@ public final class HugeLongLongDoubleMap extends PagedDataStructure<LongLongDoub
         return new HugeLongLongDoubleMap(size, ALLOCATOR_FACTORY.newAllocator(tracker));
     }
 
-    private HugeLongLongDoubleMap(long size, PageAllocator<LongLongDoubleHashMap> allocator) {
+    private HugeLongLongDoubleMap(long size, PageAllocator<NestedMap> allocator) {
         super(size, allocator);
     }
 
     public double getOrDefault(long index1, long index2, double defaultValue) {
         assert index1 < capacity();
-        final int pageIndex = pageIndex(index1);
-        return pages[pageIndex].getOrDefault(index1, index2, defaultValue);
+        int pageIndex = pageIndex(index1);
+        int indexInPage = indexInPage(index1);
+        return pages[pageIndex].get(indexInPage, index2, defaultValue);
     }
 
-    public double put(long index1, long index2, double value) {
+    public void put(long index1, long index2, double value) {
         assert index1 < capacity();
-        final int pageIndex = pageIndex(index1);
-        return pages[pageIndex].put(index1, index2, value);
+        int pageIndex = pageIndex(index1);
+        int indexInPage = indexInPage(index1);
+        pages[pageIndex].put(indexInPage, index2, value);
+    }
+
+    static final class NestedMap {
+        private final LongDoubleMap[] page;
+        private final AllocationTracker tracker;
+
+        NestedMap(final int pageSize, final AllocationTracker tracker) {
+            this.tracker = tracker;
+            page = new LongDoubleMap[pageSize];
+        }
+
+        double get(int indexInPage, long index2, double defaultValue) {
+            LongDoubleMap map = page[indexInPage];
+            return map != null ? map.getOrDefault(index2, defaultValue) : defaultValue;
+        }
+
+        void put(int indexInPage, long index2, double value) {
+            mapForIndex(indexInPage).put(index2, value);
+        }
+
+        private LongDoubleMap mapForIndex(int indexInPage) {
+            LongDoubleMap map = page[indexInPage];
+            if (map == null) {
+                map = createNewPage(indexInPage);
+            }
+            return map;
+        }
+
+        synchronized private LongDoubleMap createNewPage(final int indexInPage) {
+            LongDoubleMap map;
+            map = page[indexInPage];
+            if (map == null) {
+                map = page[indexInPage] = new TrackingLongDoubleHashMap(tracker);
+            }
+            return map;
+        }
     }
 }
---
+++ tests/src/test/java/org/neo4j/graphalgo/core/huge/HugeGraphWeightTest.java
@@ -0,0 +1,112 @@
+package org.neo4j.graphalgo.core.huge;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.neo4j.graphalgo.api.HugeGraph;
+import org.neo4j.graphalgo.core.GraphLoader;
+import org.neo4j.graphalgo.core.utils.Pools;
+import org.neo4j.graphalgo.core.utils.paged.MemoryUsage;
+import org.neo4j.graphalgo.core.utils.paged.PageUtil;
+import org.neo4j.graphdb.Direction;
+import org.neo4j.kernel.api.DataWriteOperations;
+import org.neo4j.kernel.api.Statement;
+import org.neo4j.kernel.api.TokenWriteOperations;
+import org.neo4j.kernel.api.exceptions.KernelException;
+import org.neo4j.kernel.api.properties.DefinedProperty;
+import org.neo4j.kernel.internal.GraphDatabaseAPI;
+import org.neo4j.test.rule.ImpermanentDatabaseRule;
+
+import static org.junit.Assert.assertEquals;
+
+public final class HugeGraphWeightTest {
+
+    private static final int WEIGHT_BATCH_SIZE = PageUtil.pageSizeFor(MemoryUsage.BYTES_OBJECT_REF);
+    // make sure that multiple threads are loading the same batch
+    private static final int BATCH_SIZE = 100;
+
+    @Rule
+    public ImpermanentDatabaseRule db = new ImpermanentDatabaseRule();
+
+    @Test
+    public void shouldLoadCorrectWeights() throws Exception {
+        mkDb(WEIGHT_BATCH_SIZE << 1, 2);
+        HugeGraph graph = loadGraph(db);
+
+        graph.forEachNode((long node) -> {
+            graph.forEachOutgoing(node, (src, tgt) -> {
+                long weight = (long) graph.weightOf(src, tgt);
+                int fakeId = ((int) src << 16) | (int) tgt & 0xFFFF;
+                assertEquals(
+                        "Wrong weight for (" + src + ")->(" + tgt + ")",
+                        fakeId, weight);
+                return true;
+            });
+            return true;
+        });
+    }
+
+    @Test(timeout = 10000)
+    public void shouldLoadMoreWeights() throws Exception {
+        mkDb(WEIGHT_BATCH_SIZE, 4);
+        loadGraph(db);
+    }
+
+    private void mkDb(final int nodes, final int relsPerNode) {
+        db.executeAndCommit(__ -> {
+            try (Statement st = db.statement()) {
+                TokenWriteOperations token = st.tokenWriteOperations();
+                int type = token.relationshipTypeGetOrCreateForName("TYPE");
+                int key = token.propertyKeyGetOrCreateForName("weight");
+                DataWriteOperations write = st.dataWriteOperations();
+                NewRel newRel = newRel(write, type, key);
+
+                long[] nodeIds = new long[nodes];
+                for (int i = 0; i < nodes; i++) {
+                    nodeIds[i] = write.nodeCreate();
+                }
+
+                int pageSize = PageUtil.pageSizeFor(MemoryUsage.BYTES_OBJECT_REF);
+                for (int i = 0; i < nodes; i += pageSize) {
+                    int max = Math.min(pageSize, nodes - i);
+                    for (int j = 0; j < max; j++) {
+                        long sourceId = nodeIds[i + j];
+                        for (int k = 1; k <= relsPerNode; k++) {
+                            int targetIndex = j + k;
+                            if (targetIndex >= pageSize) {
+                                targetIndex = j - k;
+                            }
+                            long targetId = nodeIds[i + targetIndex];
+                            newRel.mk(sourceId, targetId);
+                        }
+                    }
+                }
+            } catch (KernelException e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
+
+    private HugeGraph loadGraph(final GraphDatabaseAPI db) {
+        return (HugeGraph) new GraphLoader(db)
+                .withRelationshipWeightsFromProperty("weight", 0)
+                .withDirection(Direction.OUTGOING)
+                .withExecutorService(Pools.DEFAULT)
+                .withBatchSize(BATCH_SIZE)
+                .load(HugeGraphFactory.class);
+    }
+
+    private interface NewRel {
+        void mk(long source, long target) throws KernelException;
+    }
+
+    private static NewRel newRel(
+            DataWriteOperations write,
+            int type,
+            int key) {
+        return (source, target) -> {
+            int fakeId = ((int) source << 16) | (int) target & 0xFFFF;
+            long rel = write.relationshipCreate(type, source, target);
+            write.relationshipSetProperty(rel, DefinedProperty.intProperty(key, fakeId));
+        };
+    }
+}
