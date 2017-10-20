1e7e6d1076dacd039b54818a6c7ba38d960ebe63
Martin Knobloch <mknblch@users.noreply.github.com>
2017-10-20 20:36:07 +0200

    [WIP] Default triangle proc change (#435)
    Default triangle proc change

--- algo/src/main/java/org/neo4j/graphalgo/TriangleProc.java
+++ algo/src/main/java/org/neo4j/graphalgo/TriangleProc.java
@@ -68,8 +68,8 @@ public class TriangleProc {
         return triangleStream.resultStream();
     }
 
-    @Procedure("algo.triangleCount.stream")
-    @Description("CALL algo.triangleCount.stream(label, relationship, {concurrency:4}) " +
+    @Procedure("algo.triangleCount.exp1.stream")
+    @Description("CALL algo.triangleCount.exp1.stream(label, relationship, {concurrency:4}) " +
             "YIELD nodeId, triangles - yield nodeId, number of triangles")
     public Stream<TriangleCount.Result> triangleCountStream(
             @Name(value = "label", defaultValue = "") String label,
@@ -97,7 +97,7 @@ public class TriangleProc {
         return triangleCount.resultStream();
     }
 
-    @Procedure("algo.triangleCount.exp1.stream")
+    @Procedure("algo.triangleCount.stream")
     @Description("CALL algo.triangleCount.stream(label, relationship, {concurrency:8}) " +
             "YIELD nodeId, triangles - yield nodeId, number of triangles")
     public Stream<TriangleCountExp.Result> triangleCountExp1Stream(
@@ -126,8 +126,8 @@ public class TriangleProc {
         return triangleCount.resultStream();
     }
 
-    @Procedure(value = "algo.triangleCount", mode = Mode.WRITE)
-    @Description("CALL algo.triangleCount(label, relationship, " +
+    @Procedure(value = "algo.triangleCount.exp1", mode = Mode.WRITE)
+    @Description("CALL algo.triangleCount.exp1(label, relationship, " +
             "{concurrency:4, write:true, writeProperty:'triangles', clusteringCoefficientProperty:'coefficient'}) " +
             "YIELD loadMillis, computeMillis, writeMillis, nodeCount, triangleCount, averageClusteringCoefficient")
     public Stream<Result> triangleCount(
@@ -197,7 +197,7 @@ public class TriangleProc {
         return Stream.of(builder.build());
     }
 
-    @Procedure(value = "algo.triangleCount.exp1", mode = Mode.WRITE)
+    @Procedure(value = "algo.triangleCount", mode = Mode.WRITE)
     @Description("CALL algo.triangleCount(label, relationship, " +
             "{concurrency:8, write:true, writeProperty:'triangles', clusteringCoefficientProperty:'coefficient'}) " +
             "YIELD loadMillis, computeMillis, writeMillis, nodeCount, triangleCount, averageClusteringCoefficient")
@@ -207,7 +207,7 @@ public class TriangleProc {
             @Name(value = "config", defaultValue = "{}") Map<String, Object> config) {
 
         final Graph graph;
-        final TriangleCount triangleCount;
+        final TriangleCountExp triangleCount;
         final double[] clusteringCoefficients;
 
         final ProcedureConfiguration configuration = ProcedureConfiguration.create(config)
@@ -228,7 +228,7 @@ public class TriangleProc {
 
         final TerminationFlag terminationFlag = TerminationFlag.wrap(transaction);
         try (ProgressTimer timer = builder.timeEval()) {
-            triangleCount = new TriangleCount(graph, Pools.DEFAULT, configuration.getConcurrency())
+            triangleCount = new TriangleCountExp(graph, Pools.DEFAULT, configuration.getConcurrency())
                     .withProgressLogger(ProgressLogger.wrap(log, "triangleCount"))
                     .withTerminationFlag(terminationFlag)
                     .compute();
