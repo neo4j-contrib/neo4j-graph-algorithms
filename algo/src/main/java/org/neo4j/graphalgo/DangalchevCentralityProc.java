528e9dab78d7daeb3eefa2a0e7f4cad48a4509e1
Martin <mknblch@users.noreply.github.com>
2018-02-01 21:36:49 +0100

    Danglachev centrality (#549)
    * WIP
    
    * add testcase for 546
    
    * cleanup
    
    * meant 547
    
    * WIP
    
    * WIP
    
    * add proc + test
    
    * revert changes in HCC
    
    * revert unwanted changes

---
+++ algo/src/main/java/org/neo4j/graphalgo/DangalchevCentralityProc.java
@@ -0,0 +1,141 @@
+/**
+ * Copyright (c) 2017 "Neo4j, Inc." <http://neo4j.com>
+ *
+ * This file is part of Neo4j Graph Algorithms <http://github.com/neo4j-contrib/neo4j-graph-algorithms>.
+ *
+ * Neo4j Graph Algorithms is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.graphalgo;
+
+import org.neo4j.graphalgo.api.Graph;
+import org.neo4j.graphalgo.core.GraphLoader;
+import org.neo4j.graphalgo.core.ProcedureConfiguration;
+import org.neo4j.graphalgo.core.utils.Pools;
+import org.neo4j.graphalgo.core.utils.ProgressLogger;
+import org.neo4j.graphalgo.core.utils.ProgressTimer;
+import org.neo4j.graphalgo.core.utils.TerminationFlag;
+import org.neo4j.graphalgo.core.utils.paged.AllocationTracker;
+import org.neo4j.graphalgo.core.write.Exporter;
+import org.neo4j.graphalgo.impl.harmonic.HarmonicCentrality;
+import org.neo4j.graphalgo.impl.DangalchevClosenessCentrality;
+import org.neo4j.graphalgo.results.CentralityProcResult;
+import org.neo4j.graphdb.Direction;
+import org.neo4j.kernel.api.KernelTransaction;
+import org.neo4j.kernel.internal.GraphDatabaseAPI;
+import org.neo4j.logging.Log;
+import org.neo4j.procedure.*;
+
+import java.util.Map;
+import java.util.stream.Stream;
+
+/**
+ * @author mknblch
+ */
+public class DangalchevCentralityProc {
+
+    public static final String DEFAULT_TARGET_PROPERTY = "centrality";
+
+    @Context
+    public GraphDatabaseAPI api;
+
+    @Context
+    public Log log;
+
+    @Context
+    public KernelTransaction transaction;
+
+    @Procedure(value = "algo.dangalchev.stream")
+    @Description("CALL algo.dangalchev.stream(label:String, relationship:String{concurrency:4}) YIELD nodeId, centrality - yields centrality for each node")
+    public Stream<DangalchevClosenessCentrality.Result> dangalchevStream(
+            @Name(value = "label", defaultValue = "") String label,
+            @Name(value = "relationship", defaultValue = "") String relationship,
+            @Name(value = "config", defaultValue = "{}") Map<String, Object> config) {
+
+        final ProcedureConfiguration configuration = ProcedureConfiguration.create(config)
+                .overrideNodeLabelOrQuery(label)
+                .overrideRelationshipTypeOrQuery(relationship);
+
+        final AllocationTracker tracker = AllocationTracker.create();
+
+        final Graph graph = new GraphLoader(api, Pools.DEFAULT)
+                .init(log, configuration.getNodeLabelOrQuery(), configuration.getRelationshipOrQuery(), configuration)
+                .withoutNodeProperties()
+                .withConcurrency(configuration.getConcurrency())
+                .withAllocationTracker(tracker)
+                .asUndirected(true)
+                .load(configuration.getGraphImpl("huge"));
+
+        final DangalchevClosenessCentrality algo = new DangalchevClosenessCentrality(graph, configuration.getConcurrency(), Pools.DEFAULT)
+                .withProgressLogger(ProgressLogger.wrap(log, "DangalchevCentrality"))
+                .withTerminationFlag(TerminationFlag.wrap(transaction))
+                .compute();
+
+        graph.release();
+
+        return algo.resultStream();
+    }
+
+    @Procedure(value = "algo.dangalchev", mode = Mode.WRITE)
+    @Description("CALL algo.dangalchev(label:String, relationship:String, {write:true, writeProperty:'centrality, concurrency:4'}) YIELD " +
+            "loadMillis, computeMillis, writeMillis, nodes] - yields evaluation details")
+    public Stream<CentralityProcResult> dangalchev(
+            @Name(value = "label", defaultValue = "") String label,
+            @Name(value = "relationship", defaultValue = "") String relationship,
+            @Name(value = "config", defaultValue = "{}") Map<String, Object> config) {
+
+        final ProcedureConfiguration configuration = ProcedureConfiguration.create(config)
+                .overrideNodeLabelOrQuery(label)
+                .overrideRelationshipTypeOrQuery(relationship);
+
+        final CentralityProcResult.Builder builder = CentralityProcResult.builder();
+
+        final AllocationTracker tracker = AllocationTracker.create();
+        final int concurrency = configuration.getConcurrency();
+        final TerminationFlag terminationFlag = TerminationFlag.wrap(transaction);
+
+        final Graph graph;
+        try (ProgressTimer timer = builder.timeLoad()) {
+            graph = new GraphLoader(api, Pools.DEFAULT)
+                    .init(log, configuration.getNodeLabelOrQuery(), configuration.getRelationshipOrQuery(), configuration)
+                    .withoutNodeProperties()
+                    .withConcurrency(concurrency)
+                    .withAllocationTracker(tracker)
+                    .asUndirected(true)
+                    .load(configuration.getGraphImpl("huge"));
+        }
+
+        builder.withNodeCount(graph.nodeCount());
+
+        final DangalchevClosenessCentrality algo = new DangalchevClosenessCentrality(graph, concurrency, Pools.DEFAULT)
+                .withProgressLogger(ProgressLogger.wrap(log, "DangalchevCentrality"))
+                .withTerminationFlag(TerminationFlag.wrap(transaction));
+
+        builder.timeEval(algo::compute);
+
+        if (configuration.isWriteFlag()) {
+            graph.release();
+            final String writeProperty = configuration.getWriteProperty(DEFAULT_TARGET_PROPERTY);
+            builder.timeWrite(() -> {
+                Exporter exporter = Exporter.of(api, graph)
+                        .withLog(log)
+                        .parallel(Pools.DEFAULT, concurrency, terminationFlag)
+                        .build();
+                algo.export(writeProperty, exporter);
+            });
+            algo.release();
+        }
+
+        return Stream.of(builder.build());
+    }
+}
---
+++ tests/src/test/java/org/neo4j/graphalgo/algo/ClosenessCentralityIntegrationTest_546.java
@@ -0,0 +1,194 @@
+/**
+ * Copyright (c) 2017 "Neo4j, Inc." <http://neo4j.com>
+ *
+ * This file is part of Neo4j Graph Algorithms <http://github.com/neo4j-contrib/neo4j-graph-algorithms>.
+ *
+ * Neo4j Graph Algorithms is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.graphalgo.algo;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.neo4j.graphalgo.api.Graph;
+import org.neo4j.graphalgo.core.GraphLoader;
+import org.neo4j.graphalgo.core.huge.HugeGraphFactory;
+import org.neo4j.graphalgo.core.utils.Pools;
+import org.neo4j.graphalgo.impl.MSClosenessCentrality;
+import org.neo4j.graphalgo.impl.DangalchevClosenessCentrality;
+import org.neo4j.test.rule.ImpermanentDatabaseRule;
+
+/**
+ *
+ * @author mknblch
+ */
+public class ClosenessCentralityIntegrationTest_546 {
+
+    @Rule
+    public ImpermanentDatabaseRule db = new ImpermanentDatabaseRule();
+
+    private String name(long id) {
+        String[] name = {""};
+        db.execute("MATCH (n) WHERE id(n) = " + id + " RETURN n.id as name")
+                .accept(row -> {
+                    name[0] = row.getString("name");
+                    return false;
+                });
+        if (name[0].isEmpty()) {
+            throw new IllegalArgumentException("unknown id " + id);
+        }
+        return name[0];
+    }
+
+    @Test
+    public void test547() throws Exception {
+
+        String importQuery =
+                "CREATE (alice:Person{id:\"Alice\"}),\n" +
+                        "       (michael:Person{id:\"Michael\"}),\n" +
+                        "       (karin:Person{id:\"Karin\"}),\n" +
+                        "       (chris:Person{id:\"Chris\"}),\n" +
+                        "       (will:Person{id:\"Will\"}),\n" +
+                        "       (mark:Person{id:\"Mark\"})\n" +
+                        "CREATE (michael)<-[:KNOWS]-(karin),\n" +
+                        "       (michael)-[:KNOWS]->(chris),\n" +
+                        "       (will)-[:KNOWS]->(michael),\n" +
+                        "       (mark)<-[:KNOWS]-(michael),\n" +
+                        "       (mark)-[:KNOWS]->(will),\n" +
+                        "       (alice)-[:KNOWS]->(michael),\n" +
+                        "       (will)-[:KNOWS]->(chris),\n" +
+                        "       (chris)-[:KNOWS]->(karin);";
+
+        db.execute(importQuery);
+
+        final Graph graph = new GraphLoader(db, Pools.DEFAULT)
+                .withLabel("Person")
+                .withRelationshipType("KNOWS")
+                .asUndirected(true)
+                .load(HugeGraphFactory.class);
+
+        System.out.println("547:");
+        new MSClosenessCentrality(graph, 2, Pools.DEFAULT)
+                .compute()
+                .resultStream()
+                .forEach(this::print);
+
+    }
+
+    @Test
+    public void test547_residual() throws Exception {
+
+        String importQuery =
+                "CREATE (alice:Person{id:\"Alice\"}),\n" +
+                        "       (michael:Person{id:\"Michael\"}),\n" +
+                        "       (karin:Person{id:\"Karin\"}),\n" +
+                        "       (chris:Person{id:\"Chris\"}),\n" +
+                        "       (will:Person{id:\"Will\"}),\n" +
+                        "       (mark:Person{id:\"Mark\"})\n" +
+                        "CREATE (michael)<-[:KNOWS]-(karin),\n" +
+                        "       (michael)-[:KNOWS]->(chris),\n" +
+                        "       (will)-[:KNOWS]->(michael),\n" +
+                        "       (mark)<-[:KNOWS]-(michael),\n" +
+                        "       (mark)-[:KNOWS]->(will),\n" +
+                        "       (alice)-[:KNOWS]->(michael),\n" +
+                        "       (will)-[:KNOWS]->(chris),\n" +
+                        "       (chris)-[:KNOWS]->(karin);";
+
+        db.execute(importQuery);
+
+        final Graph graph = new GraphLoader(db, Pools.DEFAULT)
+                .withLabel("Person")
+                .withRelationshipType("KNOWS")
+                .asUndirected(true)
+                .load(HugeGraphFactory.class);
+
+        System.out.println("547 Dangalchev:");
+        new DangalchevClosenessCentrality(graph, 2, Pools.DEFAULT)
+                .compute()
+                .resultStream()
+                .forEach(this::print);
+
+    }
+
+    @Test
+    public void test546() throws Exception {
+
+        String importQuery =
+                "CREATE (nAlice:User {id:'Alice'})\n" +
+                        ",(nBridget:User {id:'Bridget'})\n" +
+                        ",(nCharles:User {id:'Charles'})\n" +
+                        ",(nMark:User {id:'Mark'})\n" +
+                        ",(nMichael:User {id:'Michael'})\n" +
+                        "CREATE (nAlice)-[:FRIEND]->(nBridget)\n" +
+                        ",(nAlice)<-[:FRIEND]-(nBridget)\n" +
+                        ",(nAlice)-[:FRIEND]->(nCharles)\n" +
+                        ",(nAlice)<-[:FRIEND]-(nCharles)\n" +
+                        ",(nMark)-[:FRIEND]->(nMichael)\n" +
+                        ",(nMark)<-[:FRIEND]-(nMichael);";
+
+        db.execute(importQuery);
+
+        final Graph graph = new GraphLoader(db, Pools.DEFAULT)
+                .withLabel("Person")
+                .withRelationshipType("KNOWS")
+                .asUndirected(true)
+                .load(HugeGraphFactory.class);
+
+        System.out.println("546:");
+        new MSClosenessCentrality(graph, 2, Pools.DEFAULT)
+                .compute()
+                .resultStream()
+                .forEach(this::print);
+
+    }
+
+    @Test
+    public void test546_residual() throws Exception {
+
+        String importQuery =
+                "CREATE (nAlice:User {id:'Alice'})\n" +
+                        ",(nBridget:User {id:'Bridget'})\n" +
+                        ",(nCharles:User {id:'Charles'})\n" +
+                        ",(nMark:User {id:'Mark'})\n" +
+                        ",(nMichael:User {id:'Michael'})\n" +
+                        "CREATE (nAlice)-[:FRIEND]->(nBridget)\n" +
+                        ",(nAlice)<-[:FRIEND]-(nBridget)\n" +
+                        ",(nAlice)-[:FRIEND]->(nCharles)\n" +
+                        ",(nAlice)<-[:FRIEND]-(nCharles)\n" +
+                        ",(nMark)-[:FRIEND]->(nMichael)\n" +
+                        ",(nMark)<-[:FRIEND]-(nMichael);";
+
+        db.execute(importQuery);
+
+        final Graph graph = new GraphLoader(db, Pools.DEFAULT)
+                .withLabel("Person")
+                .withRelationshipType("KNOWS")
+                .asUndirected(true)
+                .load(HugeGraphFactory.class);
+
+        System.out.println("546 Dangalchev:");
+        new DangalchevClosenessCentrality(graph, 2, Pools.DEFAULT)
+                .compute()
+                .resultStream()
+                .forEach(this::print);
+
+    }
+
+    private void print(MSClosenessCentrality.Result result) {
+        System.out.printf("%s | %.3f%n", name(result.nodeId), result.centrality);
+    }
+
+    private void print(DangalchevClosenessCentrality.Result result) {
+        System.out.printf("%s | %.3f%n", name(result.nodeId), result.centrality);
+    }
+}
