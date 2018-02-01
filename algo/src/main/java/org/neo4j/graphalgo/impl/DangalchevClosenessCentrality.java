3e3e34e2f160f48a37a13233858f5b8d61e31db0
Martin <mknblch@users.noreply.github.com>
2018-02-01 21:36:49 +0100

    Danglachev centrality (#549)
    * WIP
    
    * add testcase for 546
    
    * cleanup
    
    * meant 547
    
    * WIP
    
    * WIP
    
    * add proc + test
    
    * revert changes in HCC
    
    * revert unwanted changes

--- algo/src/main/java/org/neo4j/graphalgo/impl/DangalchevClosenessCentrality.java
+++ algo/src/main/java/org/neo4j/graphalgo/impl/DangalchevClosenessCentrality.java
@@ -19,6 +19,7 @@
 package org.neo4j.graphalgo.impl;
 
 import org.neo4j.graphalgo.api.Graph;
+import org.neo4j.graphalgo.core.utils.AtomicDoubleArray;
 import org.neo4j.graphalgo.core.utils.ProgressLogger;
 import org.neo4j.graphalgo.core.write.Exporter;
 import org.neo4j.graphalgo.core.write.PropertyTranslator;
@@ -27,41 +28,38 @@ import org.neo4j.graphalgo.impl.msbfs.MultiSourceBFS;
 import org.neo4j.graphdb.Direction;
 
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.atomic.AtomicIntegerArray;
-import java.util.function.LongToIntFunction;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 /**
- * Normalized Closeness Centrality
+ * Dangalchev Closeness Centrality
  *
  * @author mknblch
  */
-public class MSClosenessCentrality extends MSBFSCCAlgorithm<MSClosenessCentrality> {
+public class DangalchevClosenessCentrality extends Algorithm<DangalchevClosenessCentrality> {
 
     private Graph graph;
-    private AtomicIntegerArray farness;
+    private AtomicDoubleArray farness;
 
     private final int concurrency;
     private final ExecutorService executorService;
     private final int nodeCount;
 
-    public MSClosenessCentrality(Graph graph, int concurrency, ExecutorService executorService) {
+    public DangalchevClosenessCentrality(Graph graph, int concurrency, ExecutorService executorService) {
         this.graph = graph;
         nodeCount = Math.toIntExact(graph.nodeCount());
         this.concurrency = concurrency;
         this.executorService = executorService;
-        farness = new AtomicIntegerArray(nodeCount);
+        farness = new AtomicDoubleArray(nodeCount);
     }
 
-    @Override
-    public MSClosenessCentrality compute() {
+    public DangalchevClosenessCentrality compute() {
 
         final ProgressLogger progressLogger = getProgressLogger();
 
         final BfsConsumer consumer = (nodeId, depth, sourceNodeIds) -> {
             int len = sourceNodeIds.size();
-            farness.addAndGet(nodeId, len * depth);
+            farness.add(nodeId, len * 1.0 / Math.pow(2, depth));
             progressLogger.logProgress((double) nodeId / (nodeCount - 1));
         };
 
@@ -71,41 +69,33 @@ public class MSClosenessCentrality extends MSBFSCCAlgorithm<MSClosenessCentralit
         return this;
     }
 
-    @Override
     public Stream<Result> resultStream() {
-        final double k = nodeCount - 1;
         return IntStream.range(0, nodeCount)
                 .mapToObj(nodeId -> new Result(
                         graph.toOriginalNodeId(nodeId),
-                        centrality(farness.get(nodeId), k)));
+                        farness.get(nodeId)));
     }
 
-    @Override
-    public LongToIntFunction farness() {
-        return (i) -> farness.get((int) i);
-    }
-
-    @Override
     public void export(final String propertyName, final Exporter exporter) {
-        final double k = nodeCount - 1;
         exporter.write(
                 propertyName,
                 farness,
-                (PropertyTranslator.OfDouble<AtomicIntegerArray>)
-                        (data, nodeId) -> centrality(data.get((int) nodeId), k));
+                (PropertyTranslator.OfDouble<AtomicDoubleArray>)
+                        (data, nodeId) -> data.get((int) nodeId));
     }
 
     @Override
-    public MSClosenessCentrality me() {
+    public DangalchevClosenessCentrality me() {
         return this;
     }
 
     @Override
-    public MSClosenessCentrality release() {
+    public DangalchevClosenessCentrality release() {
         graph = null;
         farness = null;
         return this;
     }
+
 
     /**
      * Result class used for streaming
@@ -119,14 +109,6 @@ public class MSClosenessCentrality extends MSBFSCCAlgorithm<MSClosenessCentralit
         public Result(long nodeId, double centrality) {
             this.nodeId = nodeId;
             this.centrality = centrality;
-        }
-
-        @Override
-        public String toString() {
-            return "Result{" +
-                    "nodeId=" + nodeId +
-                    ", centrality=" + centrality +
-                    '}';
         }
     }
 }
