52bf8fd67b105a62776c6122b3bcedc3d4b6ce8d
Mark Needham <mark.needham@neotechnology.com>
2018-03-12 12:35:23 +0000

    exception moved to internal

--- tests/src/test/java/org/neo4j/graphalgo/algo/ShortestPathAStarTest.java
+++ tests/src/test/java/org/neo4j/graphalgo/algo/ShortestPathAStarTest.java
@@ -13,7 +13,7 @@ import org.neo4j.graphalgo.ShortestPathProc;
 import org.neo4j.graphalgo.TestDatabaseCreator;
 import org.neo4j.graphdb.Node;
 import org.neo4j.graphdb.Transaction;
-import org.neo4j.kernel.api.exceptions.KernelException;
+import org.neo4j.internal.kernel.api.exceptions.KernelException;
 import org.neo4j.kernel.impl.proc.Procedures;
 import org.neo4j.kernel.internal.GraphDatabaseAPI;
 
