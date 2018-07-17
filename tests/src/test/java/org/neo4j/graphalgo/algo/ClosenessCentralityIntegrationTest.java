f209d161b756bae4a2ab1e60f54ed9376fa7c060
Paul Horn <knutwalker@users.noreply.github.com>
2018-07-17 12:33:55 +0200

    Fix CC tests due #661 (#666)

--- tests/src/test/java/org/neo4j/graphalgo/algo/ClosenessCentralityIntegrationTest.java
+++ tests/src/test/java/org/neo4j/graphalgo/algo/ClosenessCentralityIntegrationTest.java
@@ -18,15 +18,14 @@
  */
 package org.neo4j.graphalgo.algo;
 
-import org.junit.AfterClass;
 import org.junit.BeforeClass;
+import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.AdditionalMatchers;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 import org.neo4j.graphalgo.ClosenessCentralityProc;
-import org.neo4j.graphalgo.TestDatabaseCreator;
 import org.neo4j.graphalgo.helper.graphbuilder.DefaultBuilder;
 import org.neo4j.graphalgo.helper.graphbuilder.GraphBuilder;
 import org.neo4j.graphdb.Node;
@@ -34,11 +33,13 @@ import org.neo4j.graphdb.RelationshipType;
 import org.neo4j.graphdb.Result;
 import org.neo4j.internal.kernel.api.exceptions.KernelException;
 import org.neo4j.kernel.impl.proc.Procedures;
-import org.neo4j.kernel.internal.GraphDatabaseAPI;
-import org.neo4j.graphalgo.TestDatabaseCreator;
+import org.neo4j.test.rule.ImpermanentDatabaseRule;
 
 import static org.junit.Assert.assertNotEquals;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.anyLong;
+import static org.mockito.Mockito.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 
 /**
@@ -49,12 +50,12 @@ public class ClosenessCentralityIntegrationTest {
 
     public static final String TYPE = "TYPE";
 
-    private static GraphDatabaseAPI db;
-    private static DefaultBuilder builder;
+    @ClassRule
+    public static ImpermanentDatabaseRule DB = new ImpermanentDatabaseRule();
+
     private static long centerNodeId;
 
     interface TestConsumer {
-
         void accept(long nodeId, double centrality);
     }
 
@@ -63,10 +64,7 @@ public class ClosenessCentralityIntegrationTest {
 
     @BeforeClass
     public static void setupGraph() throws KernelException {
-
-        db = TestDatabaseCreator.createTestDatabase();
-
-        builder = GraphBuilder.create(db)
+        final DefaultBuilder builder = GraphBuilder.create(DB)
                 .setLabel("Node")
                 .setRelationship(TYPE);
 
@@ -94,20 +92,12 @@ public class ClosenessCentralityIntegrationTest {
                     center.createRelationshipTo(node, type);
                 });
 
-        db.getDependencyResolver()
-                .resolveDependency(Procedures.class)
-                .registerProcedure(ClosenessCentralityProc.class);
-    }
-
-    @AfterClass
-    public static void tearDown() throws Exception {
-        if (db != null) db.shutdown();
+        DB.resolveDependency(Procedures.class).registerProcedure(ClosenessCentralityProc.class);
     }
 
     @Test
     public void testClosenessStream() throws Exception {
-
-        db.execute("CALL algo.closeness.stream('Node', 'TYPE') YIELD nodeId, centrality")
+        DB.execute("CALL algo.closeness.stream('Node', 'TYPE') YIELD nodeId, centrality")
                 .accept((Result.ResultVisitor<Exception>) row -> {
                     consumer.accept(
                             row.getNumber("nodeId").longValue(),
@@ -120,8 +110,7 @@ public class ClosenessCentralityIntegrationTest {
 
     @Test
     public void testClosenessWrite() throws Exception {
-
-        db.execute("CALL algo.closeness('','', {write:true, stats:true, writeProperty:'centrality'}) YIELD " +
+        DB.execute("CALL algo.closeness('','', {write:true, stats:true, writeProperty:'centrality'}) YIELD " +
                 "nodes, loadMillis, computeMillis, writeMillis")
                 .accept((Result.ResultVisitor<Exception>) row -> {
                     assertNotEquals(-1L, row.getNumber("writeMillis"));
@@ -130,11 +119,11 @@ public class ClosenessCentralityIntegrationTest {
                     return true;
                 });
 
-        db.execute("MATCH (n) WHERE exists(n.centrality) RETURN id(n) as id, n.centrality as centrality")
+        DB.execute("MATCH (n) WHERE exists(n.centrality) RETURN id(n) as id, n.centrality as centrality")
                 .accept(row -> {
                     System.out.println(
                             row.getNumber("id").longValue() + " " +
-                            row.getNumber("centrality").doubleValue());
+                                    row.getNumber("centrality").doubleValue());
                     consumer.accept(
                             row.getNumber("id").longValue(),
                             row.getNumber("centrality").doubleValue());
@@ -145,7 +134,7 @@ public class ClosenessCentralityIntegrationTest {
     }
 
     private void verifyMock() {
-        verify(consumer, times(1)).accept(eq(centerNodeId), eq(1.0));
-        verify(consumer, times(10)).accept(anyLong(), AdditionalMatchers.eq(0.58, 0.1));
+        verify(consumer, times(1)).accept(eq(centerNodeId), AdditionalMatchers.eq(1.0, 0.01));
+        verify(consumer, times(10)).accept(anyLong(), AdditionalMatchers.eq(0.588, 0.01));
     }
 }
