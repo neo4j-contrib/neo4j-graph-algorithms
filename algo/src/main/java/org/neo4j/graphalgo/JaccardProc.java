340f97ff0b4e520345cf4f4cfc5cc1a50e7693d0
Mark Needham <mark.needham@neotechnology.com>
2018-08-16 11:48:52 +0100

    parallelise jaccard

--- algo/src/main/java/org/neo4j/graphalgo/JaccardProc.java
+++ algo/src/main/java/org/neo4j/graphalgo/JaccardProc.java
@@ -1,19 +1,19 @@
 package org.neo4j.graphalgo;
 
 import com.carrotsearch.hppc.LongHashSet;
-import com.carrotsearch.hppc.LongObjectHashMap;
-import com.carrotsearch.hppc.LongObjectMap;
-import com.carrotsearch.hppc.LongSet;
-import org.neo4j.graphalgo.impl.walking.WalkResult;
+import org.neo4j.graphalgo.core.ProcedureConfiguration;
+import org.neo4j.graphalgo.core.utils.ParallelUtil;
+import org.neo4j.graphalgo.core.utils.Pools;
+import org.neo4j.graphalgo.core.utils.QueueBasedSpliterator;
+import org.neo4j.graphalgo.core.utils.TerminationFlag;
 import org.neo4j.kernel.api.KernelTransaction;
 import org.neo4j.kernel.internal.GraphDatabaseAPI;
 import org.neo4j.logging.Log;
 import org.neo4j.procedure.*;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
@@ -36,6 +36,9 @@ public class JaccardProc {
             @Name(value = "data", defaultValue = "null") List<Map<String, Object>> data,
             @Name(value = "config", defaultValue = "{}") Map<String, Object> config) {
 
+        ProcedureConfiguration configuration = ProcedureConfiguration.create(config);
+
+
         double similarityCutoff = ((Number) config.getOrDefault("similarityCutoff", -1D)).doubleValue();
         long degreeCutoff = ((Number) config.getOrDefault("degreeCutoff", 0L)).longValue();
 
@@ -43,25 +46,50 @@ public class JaccardProc {
         int length = ids.length;
         Stream<Integer> sourceIds = IntStream.range(0, length).parallel().boxed();
 
-        return jaccardStream(similarityCutoff, ids, length, sourceIds);
+        TerminationFlag terminationFlag = TerminationFlag.wrap(transaction);
+
+        int concurrency = configuration.getConcurrency();
+        return jaccardParallelStream(similarityCutoff, ids, length, sourceIds, terminationFlag, concurrency);
     }
 
-    private Stream<JaccardResult> jaccardStream(double similarityCutoff, InputData[] ids, int length, Stream<Integer> sourceIds) {
+    private Stream<JaccardResult> jaccardStream(double similarityCutoff, InputData[] ids, int length, Stream<Integer> sourceIds, TerminationFlag terminationFlag) {
         return sourceIds
-                .flatMap(idx1 -> {
-                    return IntStream.range(idx1 + 1, length).mapToObj(idx2 -> {
-                        return calculateJaccard(similarityCutoff, ids[idx1], ids[idx2]);
-                    }).filter(Objects::nonNull);
-                });
+                .flatMap(idx1 -> IntStream.range(idx1 + 1, length)
+                        .mapToObj(idx2 -> calculateJaccard(similarityCutoff, ids[idx1], ids[idx2])).filter(Objects::nonNull));
     }
 
-    private Stream<JaccardResult> jaccardParallelStream(double similarityCutoff, InputData[] ids, int length, Stream<Integer> sourceIds) {
-        return sourceIds
-                .flatMap(idx1 -> {
-                    return IntStream.range(idx1 + 1, length).mapToObj(idx2 -> {
-                        return calculateJaccard(similarityCutoff, ids[idx1], ids[idx2]);
-                    }).filter(Objects::nonNull);
-                });
+    private Stream<JaccardResult> jaccardParallelStream(double similarityCutoff, InputData[] ids, int length, Stream<Integer> sourceIdStream, TerminationFlag terminationFlag, int concurrency) {
+
+        int timeout = 100;
+        int queueSize = 1000;
+
+        int batchSize = ParallelUtil.adjustBatchSize(length, concurrency, 100);
+        Collection<Runnable> tasks = new ArrayList<>((length / batchSize) + 1);
+
+        ArrayBlockingQueue<JaccardResult> queue = new ArrayBlockingQueue<>(queueSize);
+        JaccardResult TOMB = JaccardResult.TOMB;
+
+        sourceIdStream.forEach(sourceId -> {
+            IntStream.range(sourceId + 1, length).forEach(otherId -> {
+                put(queue,calculateJaccard(similarityCutoff, ids[sourceId], ids[otherId]));
+            });
+        });
+
+
+        new Thread(() -> {
+            ParallelUtil.runWithConcurrency(concurrency, tasks, terminationFlag, Pools.DEFAULT);
+            put(queue,TOMB);
+        }).start();
+
+        QueueBasedSpliterator<JaccardResult> spliterator = new QueueBasedSpliterator<>(queue, TOMB, terminationFlag, timeout);
+        return StreamSupport.stream(spliterator, false);
+    }
+    private static <T> void put(BlockingQueue<T> queue, T items) {
+        try {
+            queue.put(items);
+        } catch (InterruptedException e) {
+            // ignore
+        }
     }
 
     private JaccardResult calculateJaccard(double similarityCutoff, InputData e1, InputData e2) {
@@ -110,6 +138,8 @@ public class JaccardProc {
         public final long intersection;
         public final double jaccard;
 
+        public static JaccardResult TOMB = new JaccardResult(-1,-1,-1,-1,-1, -1);
+
         public JaccardResult(long source1, long source2, long count1, long count2, long intersection, double jaccard) {
             this.source1 = source1;
             this.source2 = source2;
@@ -137,6 +167,25 @@ public class JaccardProc {
             }
 
             return new JaccardResult(source1, source2, count1, count2, intersection, jaccard);
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            JaccardResult that = (JaccardResult) o;
+            return source1 == that.source1 &&
+                    source2 == that.source2 &&
+                    count1 == that.count1 &&
+                    count2 == that.count2 &&
+                    intersection == that.intersection &&
+                    Double.compare(that.jaccard, jaccard) == 0;
+        }
+
+        @Override
+        public int hashCode() {
+
+            return Objects.hash(source1, source2, count1, count2, intersection, jaccard);
         }
 
         private static long calculateIntersection(LongHashSet targets1, LongHashSet targets2) {
