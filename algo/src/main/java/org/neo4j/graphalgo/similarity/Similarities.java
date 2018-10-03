b27d32de5a4215f90ec0e16d87a2c89735f0a7ce
Mark Needham <mark.needham@neotechnology.com>
2018-10-03 10:02:01 +0100

    overlap function

--- algo/src/main/java/org/neo4j/graphalgo/similarity/Similarities.java
+++ algo/src/main/java/org/neo4j/graphalgo/similarity/Similarities.java
@@ -1,18 +1,18 @@
 /**
  * Copyright (c) 2017 "Neo4j, Inc." <http://neo4j.com>
- *
+ * <p>
  * This file is part of Neo4j Graph Algorithms <http://github.com/neo4j-contrib/neo4j-graph-algorithms>.
- *
+ * <p>
  * Neo4j Graph Algorithms is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * <p>
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
+ * <p>
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -38,7 +38,7 @@ public class Similarities {
         int intersection = intersectionSet.size();
 
         long denominator = vector1.size() + vector2.size() - intersection;
-        return denominator == 0 ? 0 : (double)intersection / denominator;
+        return denominator == 0 ? 0 : (double) intersection / denominator;
     }
 
     @UserFunction("algo.similarity.cosine")
@@ -91,5 +91,19 @@ public class Similarities {
             "given two collection vectors, calculate similarity based on euclidean distance")
     public double euclideanSimilarity(@Name("vector1") List<Number> vector1, @Name("vector2") List<Number> vector2) {
         return 1.0d / (1 + euclideanDistance(vector1, vector2));
+    }
+
+    @UserFunction("algo.similarity.overlap")
+    @Description("algo.similarity.overlap([vector1], [vector2]) " +
+            "given two collection vectors, calculate overlap similarity")
+    public double overlapSimilarity(@Name("vector1") List<Number> vector1, @Name("vector2") List<Number> vector2) {
+        if (vector1 == null || vector2 == null) return 0;
+
+        HashSet<Number> intersectionSet = new HashSet<>(vector1);
+        intersectionSet.retainAll(vector2);
+        int intersection = intersectionSet.size();
+
+        long denominator = Math.min(vector1.size(), vector2.size());
+        return denominator == 0 ? 0 : (double) intersection / denominator;
     }
 }
