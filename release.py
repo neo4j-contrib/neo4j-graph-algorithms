f3ad900ad654425a1ef9f59bb7b85124f7847036
Mark Needham <mark.needham@neotechnology.com>
2018-10-09 18:19:52 +0100

    create release

--- release.py
+++ release.py
@@ -2,4 +2,5 @@ import requests
 
 print("testing...")
 
-
+data = {'tag_name': '3.5.0.0'}
+response = requests.post("https://api.github.com/repos/neo4j-contrib/neo4j-graph-algorithms/releases", data=data)
