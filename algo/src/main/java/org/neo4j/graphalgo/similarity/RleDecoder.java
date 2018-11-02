97d1eea3240930b045f49bd174fa0f1b50d2e6d7
Mark Needham <m.h.needham@gmail.com>
2018-11-02 11:38:54 +0000

    latest version

--- algo/src/main/java/org/neo4j/graphalgo/similarity/RleDecoder.java
+++ algo/src/main/java/org/neo4j/graphalgo/similarity/RleDecoder.java
@@ -7,53 +7,24 @@ public class RleDecoder {
     private RleReader item2Reader = new RleReader(new double[0]);
     private double[] item1Vector;
     private double[] item2Vector;
-    private int initialSize;
-    private boolean recomputeItem1Vector = true;
-    private boolean recomputeItem2Vector = true;
 
     public RleDecoder(int initialSize) {
-        this.initialSize = initialSize;
         item1Vector = new double[initialSize];
         item2Vector = new double[initialSize];
     }
 
     public void reset(double[] item1, double[] item2) {
-        if(!item1.equals(item1Reader.vector())) {
-            item1Reader.reset(item1);
-            Arrays.fill(item1Vector, 0);
-            recomputeItem1Vector = true;
-        } else {
-            recomputeItem1Vector = false;
-        }
-
-        if(!item2.equals(item2Reader.vector())) {
-            item2Reader.reset(item2);
-            Arrays.fill(item2Vector, 0);
-            recomputeItem2Vector = true;
-        } else {
-            recomputeItem2Vector = false;
-        }
+        item1Reader.reset(item1);
+        item2Reader.reset(item2);
     }
 
     public double[] item1() {
-        if (recomputeItem1Vector) {
-            decode(item1Reader, item1Vector);
-        }
-
+        item1Reader.readInto(item1Vector);
         return item1Vector;
     }
 
     public double[] item2() {
-        if (recomputeItem2Vector) {
-            decode(item2Reader, item2Vector);
-        }
+        item2Reader.readInto(item2Vector);
         return item2Vector;
-    }
-
-    private void decode(RleReader reader, double[] vector) {
-        for (int i = 0; i < initialSize; i++) {
-            reader.next();
-            vector[i] = reader.value();
-        }
     }
 }
