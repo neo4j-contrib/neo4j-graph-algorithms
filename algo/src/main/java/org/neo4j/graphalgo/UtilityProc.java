4c96d3ce100d8951004e6f3eb03a64424664d7f3
Mark Needham <m.h.needham@gmail.com>
2019-03-25 14:55:25 +0000

    Algo as path (#853)
    * algo.asPath
    * empty if no nodes
    * handle cumulative weights and exact weights

---
+++ algo/src/main/java/org/neo4j/graphalgo/UtilityProc.java
@@ -0,0 +1,86 @@
+/**
+ * Copyright (c) 2017 "Neo4j, Inc." <http://neo4j.com>
+ * <p>
+ * This file is part of Neo4j Graph Algorithms <http://github.com/neo4j-contrib/neo4j-graph-algorithms>.
+ * <p>
+ * Neo4j Graph Algorithms is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * <p>
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.graphalgo;
+
+import org.neo4j.graphalgo.core.ProcedureConfiguration;
+import org.neo4j.graphalgo.impl.walking.WalkPath;
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Path;
+import org.neo4j.kernel.internal.GraphDatabaseAPI;
+import org.neo4j.procedure.Context;
+import org.neo4j.procedure.Description;
+import org.neo4j.procedure.Name;
+import org.neo4j.procedure.Procedure;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class UtilityProc {
+
+    @Context
+    public GraphDatabaseService db;
+
+    @Procedure("algo.asPath")
+    @Description("CALL algo.asPath - returns a path for the provided node ids and weights")
+    public Stream<PathResult> list(
+            @Name(value = "nodeIds", defaultValue = "") List<Long> nodeIds,
+            @Name(value = "weights", defaultValue = "") List<Double> weights,
+            @Name(value = "config", defaultValue = "{}") Map<String, Object> rawConfig) {
+
+        if (nodeIds.size() <= 0) {
+            return Stream.empty();
+        }
+        long[] nodes = nodeIds.stream().mapToLong(l -> l).toArray();
+
+        ProcedureConfiguration config = ProcedureConfiguration.create(rawConfig);
+
+        if (!weights.isEmpty()) {
+            boolean cumulativeWeights = config.get("cumulativeWeights", true);
+            if (cumulativeWeights) {
+                if (nodeIds.size() != weights.size()) {
+                    throw new RuntimeException(message(weights.size(), nodeIds.size(), "number of 'nodeIds'"));
+                }
+
+                return Stream.of(new PathResult(WalkPath.toPath((GraphDatabaseAPI) db, nodes, IntStream.range(0, weights.size() - 1).mapToDouble(index -> weights.get(index + 1) - weights.get(index)).toArray())));
+            } else {
+                if (nodeIds.size() - 1 != weights.size()) {
+                    throw new RuntimeException(message(weights.size(), nodeIds.size() - 1, "number of 'nodeIds'-1"));
+                }
+                return Stream.of(new PathResult(WalkPath.toPath((GraphDatabaseAPI) db, nodes, weights.stream().mapToDouble(d -> d).toArray())));
+            }
+        } else {
+            return Stream.of(new PathResult(WalkPath.toPath((GraphDatabaseAPI) db, nodes)));
+        }
+    }
+
+    private String message(int actualSize, int expectedSize, String explanation) {
+        return String.format("'weights' contains %d values, but %d values were expected (%s)", actualSize, expectedSize, explanation);
+    }
+
+
+    public static class PathResult {
+        public final Path path;
+
+        public PathResult(Path path) {
+            this.path = path;
+        }
+    }
+}
---
+++ tests/src/test/java/org/neo4j/graphalgo/algo/UtilityProcsTest.java
@@ -0,0 +1,173 @@
+/**
+ * Copyright (c) 2017 "Neo4j, Inc." <http://neo4j.com>
+ *
+ * This file is part of Neo4j Graph Algorithms <http://github.com/neo4j-contrib/neo4j-graph-algorithms>.
+ *
+ * Neo4j Graph Algorithms is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.graphalgo.algo;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.neo4j.graphalgo.KShortestPathsProc;
+import org.neo4j.graphalgo.UtilityProc;
+import org.neo4j.graphdb.Label;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.Path;
+import org.neo4j.graphdb.Transaction;
+import org.neo4j.internal.kernel.api.exceptions.KernelException;
+import org.neo4j.kernel.impl.proc.Procedures;
+import org.neo4j.test.rule.ImpermanentDatabaseRule;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.StreamSupport;
+
+import static java.util.stream.Collectors.toList;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.matchers.JUnitMatchers.containsString;
+
+/**
+ * Graph:
+ *
+ *            (0)
+ *          /  |  \
+ *       (4)--(5)--(1)
+ *         \  /  \ /
+ *         (3)---(2)
+ *
+ * @author mknblch
+ */
+public class UtilityProcsTest {
+
+    @ClassRule
+    public static ImpermanentDatabaseRule DB = new ImpermanentDatabaseRule();
+
+    @BeforeClass
+    public static void setupGraph() throws KernelException {
+
+        final String cypher =
+                "CREATE (a:Node {name:'a'})\n" +
+                        "CREATE (b:Node {name:'b'})\n" +
+                        "CREATE (c:Node {name:'c'})\n" +
+                        "CREATE (d:Node {name:'d'})\n" +
+                        "CREATE (e:Node {name:'e'})\n" +
+                        "CREATE (f:Node {name:'f'})\n";
+
+        DB.execute(cypher);
+        DB.resolveDependency(Procedures.class).registerProcedure(UtilityProc.class);
+    }
+
+
+    @Test
+    public void shouldReturnPaths() throws Exception {
+        final String cypher = "CALL algo.asPath([0, 1,2])";
+
+        List<Node> expectedNodes = getNodes("a", "b", "c");
+
+
+        DB.execute(cypher).accept(row -> {
+            Path path = (Path) row.get("path");
+
+            List<Node> actualNodes = StreamSupport.stream(path.nodes().spliterator(), false).collect(toList());
+
+            assertEquals(expectedNodes, actualNodes);
+
+            return true;
+        });
+    }
+
+    @Test
+    public void shouldReturnPathsWithCosts() throws Exception {
+        final String cypher = "CALL algo.asPath([0,1,2], [0.1,0.2], {cumulativeWeights: false})";
+
+        List<Node> expectedNodes = getNodes("a", "b", "c");
+        List<Double> expectedCosts = Arrays.asList(0.1, 0.2);
+
+        DB.execute(cypher).accept(row -> {
+            Path path = (Path) row.get("path");
+
+            List<Node> actualNodes = StreamSupport.stream(path.nodes().spliterator(), false).collect(toList());
+            List<Double> actualCosts = StreamSupport.stream(path.relationships().spliterator(), false)
+                    .map(rel -> (double)rel.getProperty("cost")).collect(toList());
+
+            assertEquals(expectedNodes, actualNodes);
+            assertEquals(expectedCosts, actualCosts);
+
+            return true;
+        });
+    }
+
+    @Rule public ExpectedException exception = ExpectedException.none();
+
+    @Test
+    public void shouldThrowExceptionIfNotEnoughCostsProvided() throws Exception {
+        final String cypher = "CALL algo.asPath([0,1,2], [0.1], {cumulativeWeights: false})";
+
+        exception.expect(RuntimeException.class);
+        exception.expectMessage(CoreMatchers.containsString("'weights' contains 1 values, but 2 values were expected"));
+
+        DB.execute(cypher).close();
+    }
+
+    @Test
+    public void shouldPreprocessCumulativeWeights() throws Exception {
+        final String cypher = "CALL algo.asPath([0,1,2], [0, 40.0, 70.0])";
+
+        List<Node> expectedNodes = getNodes("a", "b", "c");
+        List<Double> expectedCosts = Arrays.asList(40.0, 30.0);
+
+        DB.execute(cypher).accept(row -> {
+            Path path = (Path) row.get("path");
+
+            List<Node> actualNodes = StreamSupport.stream(path.nodes().spliterator(), false).collect(toList());
+            List<Double> actualCosts = StreamSupport.stream(path.relationships().spliterator(), false)
+                    .map(rel -> (double)rel.getProperty("cost")).collect(toList());
+
+            assertEquals(expectedNodes, actualNodes);
+            assertEquals(expectedCosts, actualCosts);
+
+            return true;
+        });
+    }
+
+    @Test
+    public void shouldThrowExceptionIfNotEnoughCumulativeWeightsProvided() throws Exception {
+        final String cypher = "CALL algo.asPath([0,1,2], [0, 40.0])";
+
+        exception.expect(RuntimeException.class);
+        exception.expectMessage(CoreMatchers.containsString("'weights' contains 2 values, but 3 values were expected"));
+
+        DB.execute(cypher).close();
+    }
+
+    private List<Node> getNodes(String... nodes) {
+        List<Node> nodeIds;
+        try (Transaction tx = DB.beginTx()) {
+            nodeIds = Arrays.stream(nodes)
+                    .map(name -> DB.findNode(Label.label("Node"), "name", name))
+                    .collect(toList());
+        }
+        return nodeIds;
+
+    }
+
+}
